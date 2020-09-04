08b1288a64806707f1a255b148fd45e43ff55767
abasworm <worm.abas2@gmail.com>
2020-09-04 21:25:57 +0700

    Update db_pos.sql

--- db_pos.sql
+++ db_pos.sql
@@ -4,64 +4,106 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-DROP TABLE IF EXISTS `c_user`;
+CREATE TABLE IF NOT EXISTS `c_member` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` varchar(36) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `phone` varchar(30) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `last_visit` datetime DEFAULT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `is_deleted` enum('Y','N') DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `created_by` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uid` (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 CREATE TABLE IF NOT EXISTS `c_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) DEFAULT NULL,
   `username` varchar(30) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `firstname` varchar(100) NOT NULL,
-  `lastname` varchar(100) DEFAULT 'User',
+  `name` varchar(100) NOT NULL,
+  `phone` varchar(100) NOT NULL,
+  `pin` varchar(255) DEFAULT NULL,
   `other_role` varchar(255) DEFAULT NULL,
   `user_group` int(11) NOT NULL,
   `is_deleted` enum('Y','N') NOT NULL DEFAULT 'N',
   `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modified_date` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=latin1;
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `c_user_engineer`;
-CREATE TABLE IF NOT EXISTS `c_user_engineer` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(200) NOT NULL,
-  `eg_code` varchar(50) NOT NULL,
-  `uid` varchar(200) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `name` varchar(150) NOT NULL,
-  `phone` varchar(20) DEFAULT NULL,
-  `partner` varchar(50) DEFAULT NULL,
-  `street` varchar(255) DEFAULT NULL,
-  `postal_code` varchar(50) DEFAULT NULL,
-  `city` varchar(50) DEFAULT NULL,
-  `area_code` varchar(100) NOT NULL,
-  `fsl_code` varchar(50) NOT NULL,
-  `customer_no` varchar(50) NOT NULL,
-  `reset_token` varchar(200) DEFAULT NULL,
-  `reset_time` datetime DEFAULT NULL,
-  `user_group` int(11) NOT NULL DEFAULT '9',
-  `other_role` varchar(100) DEFAULT NULL,
-  `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `created_by` varchar(50) DEFAULT NULL,
-  `modified_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `modified_by` varchar(50) DEFAULT NULL,
-  `last_login_info` varchar(400) NOT NULL DEFAULT '',
-  `is_active` enum('Y','N') DEFAULT 'N',
-  `is_deleted` enum('Y','N') DEFAULT 'N',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`),
-  UNIQUE KEY `uid` (`uid`),
-  UNIQUE KEY `customer_no` (`customer_no`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
-
-DROP TABLE IF EXISTS `c_user_group`;
 CREATE TABLE IF NOT EXISTS `c_user_group` (
   `id_group` int(11) NOT NULL AUTO_INCREMENT,
   `group_name` varchar(100) NOT NULL,
   `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modified_date` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id_group`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `m_kategori` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` varchar(36) NOT NULL,
+  `kategori` varchar(100) NOT NULL,
+  `is_deleted` enum('Y','N') NOT NULL DEFAULT 'N',
+  `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` varchar(50) NOT NULL,
+  `modified_date` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `modified_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uid` (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `m_produk` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` varchar(36) NOT NULL,
+  `nama` varchar(150) NOT NULL,
+  `sku` varchar(50) NOT NULL,
+  `kategori_id` int(11) NOT NULL DEFAULT '0',
+  `produk_favorit` enum('Y','N') NOT NULL DEFAULT 'N',
+  `harga_modal` int(11) NOT NULL DEFAULT '0',
+  `harga_jual` int(11) NOT NULL DEFAULT '0',
+  `satuan` varchar(20) NOT NULL DEFAULT 'Pcs',
+  `image_path` varchar(150) DEFAULT NULL,
+  `is_active` enum('Y','N') NOT NULL DEFAULT 'Y',
+  `is_deleted` enum('Y','N') NOT NULL DEFAULT 'N',
+  `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` varchar(50) NOT NULL,
+  `modified_date` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `modified_by` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uid` (`uid`),
+  UNIQUE KEY `sku` (`sku`),
+  KEY `FK_KATEGORI_ID` (`kategori_id`),
+  CONSTRAINT `FK_KATEGORI_ID` FOREIGN KEY (`kategori_id`) REFERENCES `m_kategori` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `m_varian` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` varchar(36) NOT NULL,
+  `produk_id` int(11) NOT NULL,
+  `varian` varchar(100) NOT NULL,
+  `is_deleted` enum('Y','N') NOT NULL DEFAULT 'N',
+  `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` varchar(50) NOT NULL,
+  `modified_date` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `modified_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE KEY `uid` (`uid`),
+  KEY `FK_PRODUK_ID` (`produk_id`),
+  CONSTRAINT `FK_PRODUK_ID` FOREIGN KEY (`produk_id`) REFERENCES `m_produk` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `session_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `expires` int(11) unsigned NOT NULL,
+  `data` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
+  PRIMARY KEY (`session_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
